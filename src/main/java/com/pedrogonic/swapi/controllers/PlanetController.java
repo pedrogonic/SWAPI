b2c10f36c189e74d861ab661946907ee34c29a83
Pedro Coelho <pedrogonic@gmail.com>
2020-01-19 19:02:28 -0300

    Adding specification

---
+++ src/main/java/com/pedrogonic/swapi/controllers/PlanetController.java
@@ -0,0 +1,25 @@
+package com.pedrogonic.swapi.controllers;
+
+import com.pedrogonic.swapi.domain.Planet;
+
+public class PlanetController {
+
+    // TODO
+    void listAll() {}
+
+    // TODO
+    void searchByName(String name) {}
+
+    // TODO
+    void searchById(String id) {}
+
+    // TODO
+    void updatePlanet(String id, Planet planet) {}
+
+    // TODO
+    void createPlanet(Planet planet) {}
+
+    // TODO
+    void deletePlanet(String id) {}
+
+}
---
+++ src/main/java/com/pedrogonic/swapi/domain/Planet.java
@@ -0,0 +1,12 @@
+package com.pedrogonic.swapi.domain;
+
+public class Planet {
+
+    private String id;
+
+    private String name;
+    private String climate;
+    private String terrain;
+    private int filmCount;
+
+}
