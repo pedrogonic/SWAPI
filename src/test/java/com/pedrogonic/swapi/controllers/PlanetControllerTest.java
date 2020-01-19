dc7c179866339a694f623f0b57b9ea8e9b5c0373
Pedro Coelho <pedrogonic@gmail.com>
2020-01-19 20:29:27 -0300

    Adding Controller Test stub methods

---
+++ src/test/java/com/pedrogonic/swapi/controllers/PlanetControllerTest.java
@@ -0,0 +1,44 @@
+package com.pedrogonic.swapi.controllers;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlanetControllerTest {
+
+    // TODO
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void listAll() {
+    }
+
+    @Test
+    void searchByName() {
+    }
+
+    @Test
+    void searchById() {
+    }
+
+    @Test
+    void updatePlanet() {
+    }
+
+    @Test
+    void createPlanet() {
+    }
+
+    @Test
+    void deletePlanet() {
+    }
+}
