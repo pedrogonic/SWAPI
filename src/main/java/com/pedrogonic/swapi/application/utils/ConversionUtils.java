0dfad1cc905c1d30d9efaaedd526e4401381214a
Pedro Coelho <pedrogonic@gmail.com>
2020-01-20 15:36:52 -0300

    Adding javadocs

--- src/main/java/com/pedrogonic/swapi/application/utils/ConversionUtils.java
+++ src/main/java/com/pedrogonic/swapi/application/utils/ConversionUtils.java
@@ -4,6 +4,18 @@ import org.bson.types.ObjectId;
 
 public class ConversionUtils {
 
+    /**
+     * Converts a String to a valid ObjectId
+     * <p>
+     * If the given Id is String representation of an ObjectId, it returns an ObjectId object in which its toString
+     * method is the given String.
+     * </p>
+     * <p>
+     * Otherwise, it creates a new ObjectId object with a random value.
+     * </p>
+     * @param id
+     * @return a valid ObjectId
+     */
     public static ObjectId stringToObjectId(String id) {
         try {
             return new ObjectId(id);
--- src/main/java/com/pedrogonic/swapi/services/IPlanetService.java
+++ src/main/java/com/pedrogonic/swapi/services/IPlanetService.java
@@ -9,10 +9,62 @@ import java.util.List;
 
 public interface IPlanetService {
 
+    /**
+     * Returns planets that matches the provided filter or, if not provided, a sorted list
+     * <p>
+     * If a filled Planet Filter is provided, this method searches the repository for planets that match the Name and
+     * Id fields in the filter.
+     * </p>
+     * <p>
+     * If both fields in the Filter are null, it finds all records by the Pageable object.
+     * </p>
+     * @param pageable - must not be null
+     * @param planetFilter - must not be null
+     * @return List of Planets matching the filter or restricted list by Pageable object
+     */
     List<Planet> findAll(Pageable pageable, PlanetFilter planetFilter);
+
+    /**
+     * Return planet with provided Id
+     * <p>
+     * If no planet is found, throws  PlanetNotFoundException.
+     * </p>
+     * @param id - must not be null.
+     * @return Planet object with the given id.
+     * @throws PlanetNotFoundException - if no planet is found
+     */
     Planet findById(String id) throws PlanetNotFoundException;
+
+    /**
+     * Updates an existing Planet
+     * <p>
+     * TODO: decide update behavior
+     * </p>
+     * @param planet - must not be null
+     * @return persisted Planet
+     */
     Planet updatePlanet(Planet planet);
+
+    /**
+     * Creates a Planet
+     * <p>
+     * TODO: decide create behavior
+     * </p>
+     * @param planet - must not be null
+     * @return
+     */
     Planet createPlanet(Planet planet);
+
+    /**
+     * Deletes a Planet by Id
+     * <p>
+     * Deletes the entity with the given id.
+     * </p>
+     * <p>
+     * If no entity is found with this id, it does nothing
+     * </p>
+     * @param id - must not be null
+     */
     void deletePlanetById(String id);
 
 }
