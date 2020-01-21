72cbf0b7207934c02c3592e0ef8cffaaffb51c4c
Pedro Coelho <pedrogonic@gmail.com>
2020-01-20 23:15:52 -0300

    Adding Swagger docs

--- src/main/java/com/pedrogonic/swapi/controllers/MainController.java
+++ src/main/java/com/pedrogonic/swapi/controllers/MainController.java
@@ -2,8 +2,10 @@ package com.pedrogonic.swapi.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
+@ApiIgnore
 public class MainController {
 
     @GetMapping(value = "/")
