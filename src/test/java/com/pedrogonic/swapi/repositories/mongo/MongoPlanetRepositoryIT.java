3d518c0053562f3d65724a13d8075a87f39d9ee9
Pedro Coelho <pedrogonic@gmail.com>
2020-01-26 20:50:38 -0300

    Finishing Repository tests

--- src/test/java/com/pedrogonic/swapi/repositories/mongo/MongoPlanetRepositoryIT.java
+++ src/test/java/com/pedrogonic/swapi/repositories/mongo/MongoPlanetRepositoryIT.java
@@ -2,18 +2,23 @@ package com.pedrogonic.swapi.repositories.mongo;
 
 import com.pedrogonic.swapi.application.components.OrikaMapper;
 import com.pedrogonic.swapi.domain.Planet;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import com.pedrogonic.swapi.model.dtos.db.mongo.MongoPlanet;
+import com.pedrogonic.swapi.model.filters.PlanetFilter;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.validation.ConstraintViolationException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -34,22 +39,75 @@ class MongoPlanetRepositoryIT {
     @Autowired
     MongoPlanetRepository mongoPlanetRepository;
 
-    @BeforeEach
-    void setUp() {
+    @AfterEach
+    void tearDown() {
+        mongoTemplate.dropCollection(MongoPlanet.class);
     }
 
-    @Disabled
     @Test
+    @DisplayName("Find all")
     void findAll() {
+        Planet planet = Planet.builder()
+                .name("Tatooine")
+                .climate("arid")
+                .terrain("desert")
+                .build();
+
+        mongoPlanetRepository.insert(planet);
+
+        Pageable pageable = PageRequest.of(0, 10);
+        PlanetFilter planetFilter = PlanetFilter.builder().build();
+
+        assertEquals(1, mongoPlanetRepository.findAll(pageable, planetFilter).size());
     }
 
-    @Disabled
     @Test
-    void findById() {
+    @DisplayName("Find by existing name")
+    void findByExistingName() {
+        final String PLANET_NAME = "Tatooine";
+        final String PLANET_CLIMATE = "arid";
+        final String PLANET_TERRAIN = "desert";
+        Planet planet = Planet.builder()
+                .name(PLANET_NAME)
+                .climate(PLANET_CLIMATE)
+                .terrain(PLANET_TERRAIN)
+                .build();
+
+        mongoPlanetRepository.insert(planet);
+
+        Pageable pageable = PageRequest.of(0, 10);
+        PlanetFilter planetFilter = PlanetFilter.builder().name(PLANET_NAME).build();
+
+        List<Planet> result = mongoPlanetRepository.findAll(pageable, planetFilter);
+
+        assertEquals(1, result.size());
+        assertEquals(result.get(0).getName(), PLANET_NAME);
+        assertEquals(result.get(0).getClimate(), PLANET_CLIMATE);
+        assertEquals(result.get(0).getTerrain(), PLANET_TERRAIN);
     }
 
     @Test
-    void save() {
+    @DisplayName("Find by non existing name")
+    void findByNonExistingName() {
+        Planet planet = Planet.builder()
+                .name("Tatooine")
+                .climate("arid")
+                .terrain("desert")
+                .build();
+
+        mongoPlanetRepository.insert(planet);
+
+        Pageable pageable = PageRequest.of(0, 10);
+        PlanetFilter planetFilter = PlanetFilter.builder().name("Endor").build();
+
+        List<Planet> result = mongoPlanetRepository.findAll(pageable, planetFilter);
+
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    @DisplayName("Insert new, find by existing id and update existing")
+    void insertAndSaveAndFindById() {
         Planet planet = Planet.builder()
                 .name("Tatooine")
                 .climate("arid")
@@ -78,13 +136,41 @@ class MongoPlanetRepositoryIT {
         assertEquals(insertedPlanet, updatedPlanet);
     }
 
-    @Disabled
     @Test
-    void insert() {
+    @DisplayName("Insert invalid")
+    void insertInvalid() {
+        Planet planet = Planet.builder()
+                .name("")
+                .climate("")
+                .terrain("")
+                .build();
+
+        assertThrows(ConstraintViolationException.class,
+                () -> mongoPlanetRepository.insert(planet));
     }
 
-    @Disabled
     @Test
+    @DisplayName("Find by non existing id")
+    void findByNonExistingId() {
+        String id = "NON_EXISTING_ID";
+        assertFalse(mongoPlanetRepository.findById(id).isPresent());
+    }
+
+    @Test
+    @DisplayName("Delete by id")
     void deleteById() {
+        Planet planet = Planet.builder()
+                .name("Tatooine")
+                .climate("arid")
+                .terrain("desert")
+                .build();
+
+        Planet insertedPlanet = mongoPlanetRepository.insert(planet);
+
+        String id = insertedPlanet.getId();
+
+        mongoPlanetRepository.deleteById(id);
+
+        assertTrue(mongoPlanetRepository.findById(id).isEmpty());
     }
 }
